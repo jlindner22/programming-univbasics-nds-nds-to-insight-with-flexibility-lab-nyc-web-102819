66141b8560a96a26b874e4c64ac2f467844d3bf8
Jen Lindner <jenlindner22@gmail.com>
2019-10-26 20:30:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -69,7 +69,7 @@ end
 end
 
 def movies_with_directors_set(source)
-  source.map
+  source.map{|movie| movies_with_director_key
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
